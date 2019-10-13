4090439780502dcf8370dfa74a92843d55bd4d8c
Daviel Frias Garcia <yfriasgarcia@gmail.com>
2019-10-13 13:45:40 +0200

    deleteting all file useless and creating a boilerplate for the next app

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
-import Indecision from './components/Indecision';
+
 import 'normalize.css/normalize.css'
 import './styles/styles.scss';
-ReactDOM.render(< Indecision />, document.getElementById('app'));
+ReactDOM.render(<p>this is a Boiler plate</p>, document.getElementById('app'));
