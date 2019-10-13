2aa1a512cfa89161b52fbbf9959931ce12238977
Daviel Frias Garcia <yfriasgarcia@gmail.com>
2019-10-13 19:37:56 +0200

    creation of components and factorising the app

--- src/app.js
+++ src/app.js
@@ -1,46 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import AppRouter from './routers/AppRouters';
 import 'normalize.css/normalize.css'
 import './styles/styles.scss';
 
 
-const ExpenseDashboardPage = () => (
-    <div>
-        this is from my dashbord component
-    </div>
-);
 
-const AddExpensePage = () => (
-    <div>
-        this is from my AddExpensePage component
-    </div>
-);
-const EditPage = () => (
-    <div>
-        this is from my Edit component
-    </div>
-);
-const HelpPage = () => (
-    <div>
-        this is from my Help component
-    </div>
-);
-const NotFoundPage = () => (
-    <div>
-        404!
-    </div>
-);
-
-const routes = (
-    <BrowserRouter>
-        <Switch>
-            <Route path="/" component={ExpenseDashboardPage} exact={true} />
-            <Route path="/create" component={AddExpensePage} />
-            <Route path="/edit" component={EditPage} />
-            <Route path="/help" component={HelpPage} />
-            <Route component={NotFoundPage} />
-        </Switch>
-    </BrowserRouter>
-);
-ReactDOM.render(routes, document.getElementById('app'));
+ReactDOM.render(<AppRouter />, document.getElementById('app'));
---
+++ src/components/EditPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const EditPage = () => (
+    <div>
+        this is from my Edit component
+    </div>
+);
+
+export default EditPage;
