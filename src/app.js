8c627167472c95cf5c2964d892799ae08cc0dd11
Daviel Frias Garcia <yfriasgarcia@gmail.com>
2019-10-14 12:12:51 +0200

    added module redux@3.7.2 and added a folder called playground

--- src/app.js
+++ src/app.js
@@ -4,6 +4,4 @@ import AppRouter from './routers/AppRouters';
 import 'normalize.css/normalize.css'
 import './styles/styles.scss';
 
-
-
 ReactDOM.render(<AppRouter />, document.getElementById('app'));
--- src/components/EditPage.js
+++ src/components/EditPage.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
-const EditPage = () => (
-    <div>
-        this is from my Edit component
-    </div>
-);
+const EditPage = (props) => {
+    console.log(props);
+    return (
+        <div>
+            Editing the expense with id of {props.match.params.id}
+        </div>
+    );
+};
 
 export default EditPage;
