34041a2a0d14508814b1d9b6b32b6e4f359e39e0
Daviel Frias Garcia <yfriasgarcia@gmail.com>
2019-10-16 13:39:06 +0200

    creation of reducer V7

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -133,7 +133,16 @@ const getVisibleExpense = (expense, { text, sortBy, startDate, endDate }) => {
         const endDateMatch = typeof endDate !== 'number' || expense.createAt <= endDate;
         const textMatch = expense.description.toLowerCase().includes(text.toLowerCase());
         return startDateMatch && endDateMatch && textMatch
-    });
+    }).sort((a, b) => {
+        if (sortBy === 'date') {
+            return a.createAt < b.createAt ? 1 : -1
+
+        } else if (sortBy == 'amount') {
+            return a.amount < b.amount ? 1 : -1
+
+        }
+
+    })
 }
 
 
@@ -151,16 +160,16 @@ store.subscribe(() => {
     console.log(visibleExpense);
 });
 
-const expenseOne = store.dispatch(addExpense({ description: 'rent', amount: 100, createAt: 1000 }));
+const expenseOne = store.dispatch(addExpense({ description: 'rent', amount: 100, createAt: -21000 }));
 const expenseTwo = store.dispatch(addExpense({ description: 'coffe', amount: 300, createAt: -1000 }));
 
 // store.dispatch(removeExpense({ id: expenseOne.expense.id }));
 // store.dispatch(editExpense(expenseTwo.expense.id, { amount: 500 }));
 
-store.dispatch(setTextFilter('rent'));
+//store.dispatch(setTextFilter('rent'));
 // store.dispatch(setTextFilter());
 
-// store.dispatch(sortByAmount());
+store.dispatch(sortByAmount());
 // store.dispatch(sortByDate());
 
 //store.dispatch(setStartDate(0));
