2debf4e860d96b6e7422d1a32630e37e37db91c2
Daviel Frias Garcia <yfriasgarcia@gmail.com>
2019-10-21 20:26:33 +0200

    allows to sort by date and amountv2

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -4,13 +4,20 @@ import { setTextFilter, sortByAmount, sortByDate } from '../actions/filters';
 
 const ExpenseListFilters = (props) => (
     <div>
-        <input type="text" value={props.filters.text} onChange={(e) => {
-            props.dispatch(setTextFilter(e.target.value));
-        }} />
-        <select onChange={(e) => {
-            e.target.value === 'date' ? props.dispatch(sortByDate(e.target.value)) : props.dispatch(sortByAmount(e.target.value));
+        <input
+            type="text"
+            value={props.filters.text}
+            onChange={(e) => {
+                props.dispatch(setTextFilter(e.target.value));
+            }}
+        />
+        <select
+            value={props.filters.sortBy}
+            onChange={(e) => {
+                e.target.value === 'date' ? props.dispatch(sortByDate(e.target.value)) : props.dispatch(sortByAmount(e.target.value));
 
-        }}>
+            }}
+        >
             <option value="date">Date</option>
             <option value="amount">amount</option>
         </select>
