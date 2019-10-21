621cb4cbb77bf2d3345bba2a43efa3f0d79eb089
Daviel Frias Garcia <yfriasgarcia@gmail.com>
2019-10-21 16:27:42 +0200

    redering expenses to the screen V2

--- src/components/ExpenseDashboardPage.js
+++ src/components/ExpenseDashboardPage.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import ExpenseList from './ExpenseList'
+import ExpenseList from './ExpenseList';
+import ExpenseListFilters from './expenseListFilter';
 
 
 
 const ExpenseDashboardPage = () => (
     <div>
+        <ExpenseListFilters />
         <ExpenseList />
+
     </div>
 );
 
