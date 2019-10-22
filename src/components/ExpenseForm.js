1143578561217f9f385b5edbfda21b1540762869
Daviel Frias Garcia <yfriasgarcia@gmail.com>
2019-10-22 14:06:25 +0200

    allow the user pick a date with moment et react-dates

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import moment from 'moment';
+import { SingleDatePicker } from 'react-dates';
+import 'react-dates/lib/css/_datepicker.css';
+
+
+const now = moment();
+console.log(now.format('MMM Do YYYY'));
 class ExpenseFrom extends React.Component {
     state = {
         description: '',
         note: '',
-        amount: ''
+        amount: '',
+        createdAt: moment(),
+        calendarFocused: false
     }
 
     onDescriptionChange = (e) => {
@@ -19,7 +28,15 @@ class ExpenseFrom extends React.Component {
         if (amount.match(/^\d*(\.\d{0,2})?$/)) {
             this.setState(() => ({ amount }))
         }
-    }
+    };
+
+    onDateChange = (createdAt) => {
+        this.setState(() => ({ createdAt }));
+    };
+    onFocusChange = ({ focused }) => {
+        this.setState(() => ({ calendarFocused: focused }));
+
+    };
     render() {
         return (
             <div>
@@ -36,6 +53,14 @@ class ExpenseFrom extends React.Component {
                         placeholder="Amount"
                         value={this.state.amount}
                         onChange={this.onAmountChange}
+                    />
+                    <SingleDatePicker
+                        date={this.state.createdAt}
+                        onDateChange={this.onDateChange}
+                        focused={this.state.calendarFocused}
+                        onFocusChange={this.onFocusChange}
+                        numberOfMonths={1}
+                        isOutsideRange={(day) => false}
                     />
                     <textarea
                         placeholder="Add a note for your expense(optional)"
