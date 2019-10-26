00af3dc7a998c1a709fc4ad07d5fff0bcd7bc62f
Daviel Frias Garcia <yfriasgarcia@gmail.com>
2019-10-26 13:56:36 +0200

    filtering by date V4

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -36,11 +36,12 @@ class ExpenseListFilters extends React.Component {
                     <option value="amount">amount</option>
                 </select>
                 <DateRangePicker
-                    startDate={this.props.filters.starDate}
+                    startDate={this.props.filters.startDate}
                     endDate={this.props.filters.endDate}
                     onDatesChange={this.onDatesChange}
                     focusedInput={this.state.calendarFocused}
                     onFocusChange={this.onFocusChange}
+                    showClearDates={true}
                     numberOfMonths={1}
                     isOutsideRange={() => false}
                 />
