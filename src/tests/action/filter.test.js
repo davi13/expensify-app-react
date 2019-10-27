c743f67fa8625f2056d0882a29b748d648c69445
Daviel Frias Garcia <yfriasgarcia@gmail.com>
2019-10-27 19:58:40 +0100

    Added a test for filter  action

---
+++ src/tests/action/filter.test.js
@@ -0,0 +1,17 @@
+import moment from 'moment';
+import { setTextFilter, sortByDate, sortByAmount, setStartDate, setEndDate } from '../../actions/filters';
+
+test('Should generate set start date action object', () => {
+    const action = setStartDate(moment(0));
+    expect(action).toEqual({
+        type: 'SET_START_DATE',
+        startDate: moment(0)
+    })
+});
+test('Should generate set end date action object', () => {
+    const action = setEndDate(moment(0));
+    expect(action).toEqual({
+        type: 'SET_END_DATE',
+        endDate: moment(0)
+    });
+})
