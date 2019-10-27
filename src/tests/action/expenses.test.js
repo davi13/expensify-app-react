4128eb02cc2a961d69cf0529baecff1bccd813bd
Daviel Frias Garcia <yfriasgarcia@gmail.com>
2019-10-27 19:16:17 +0100

    Added a test for editExpense  action

--- src/tests/action/expenses.test.js
+++ src/tests/action/expenses.test.js
@@ -6,3 +6,14 @@ test('should setup remove expense action object', () => {
         id: '123abc'
     });
 });
+
+test('should setup edit an expense action object', () => {
+    const action = editExpense('123abc', { notes: 'new note Value' });
+    expect(action).toEqual({
+        type: 'EDIT_EXPENSE',
+        id: '123abc',
+        updates: {
+            notes: 'new note Value'
+        }
+    });
+});
