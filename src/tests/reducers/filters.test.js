2e031ab684292d0aa748c0679e202a98faf99876
Daviel Frias Garcia <yfriasgarcia@gmail.com>
2019-11-01 21:32:40 +0100

    added a test reducers folder V2

--- src/tests/reducers/filters.test.js
+++ src/tests/reducers/filters.test.js
@@ -10,3 +10,22 @@ test('should set up default filter values', () => {
         endDate: moment().endOf('month')
     });
 });
+
+test('Should set sortBy to amount', () => {
+    const state = filtersReducer(undefined, { type: 'SORT_BY_AMOUNT' });
+    expect(state.sortBy).toBe('amount');
+});
+
+test('should set sortBy to date', ()=>{
+    const currentState ={
+        text:'',
+        startDate:undefined,
+        endDate:undefined,
+        sortBy:'amount'
+
+    }
+    const action = {type: 'SORT_BY_DATE'}
+    const state =filtersReducer(currentState, action);
+    expect(state.sortBy).toBe('date')
+
+});
