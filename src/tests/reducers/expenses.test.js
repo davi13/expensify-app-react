f69e8b01d693051f2539860775f2d295f1e89580
Daviel Frias Garcia <yfriasgarcia@gmail.com>
2019-11-02 20:46:28 +0100

    added module react-test-renderer@16.0.0

--- src/tests/reducers/expenses.test.js
+++ src/tests/reducers/expenses.test.js
@@ -39,3 +39,29 @@ test('should add an expense', () => {
     const state = expensesReducer(expenses, action);
     expect(state).toEqual([...expenses, expense]);
 });
+
+test('should adit an expense', () => {
+    const amount = 29000
+    const action = {
+        type: 'EDIT_EXPENSE',
+        id: expenses[1].id,
+        updates:{
+            amount
+
+        }
+    }
+    const state = expensesReducer(expenses, action);
+    expect(state[1].amount).toBe(amount);
+});
+
+test('should not edit if expense not found', () => {
+    const amount = 29000
+    const action = {
+        type: 'EDIT_EXPENSE',
+        id: -1, updates:{
+            amount
+        }
+    }
+    const state = expensesReducer(expenses, action);
+    expect(state).toEqual(expenses);
+});
