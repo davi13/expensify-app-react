d830a951d3cc7074760f51401bc3e62a007ff21a
Daviel Frias Garcia <yfriasgarcia@gmail.com>
2019-11-02 21:26:13 +0100

    added module enzyme-to-json@3.0.1

--- src/tests/Component/Header.test.js
+++ src/tests/Component/Header.test.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from 'react';
+import {shallow} from 'enzyme';
 import ReactShallowRenderer from 'react-test-renderer/shallow';
 import Header from'../../components/header';
 
 test('should render header correctly', ()=>{
-    const renderer=new ReactShallowRenderer();
-    renderer.render(<Header />);
+    const wrapper =shallow(<Header />);
+    expect(wrapper).toMatchSnapshot();
 
-    expect(renderer.getRenderOutput()).toMatchSnapshot();
-    console.log(renderer.getRenderOutput());
+    //expect(wrapper.find('h1').text()).toBe('Expensify');
+    // const renderer=new ReactShallowRenderer();
+    // renderer.render(<Header />);
+
+    // expect(renderer.getRenderOutput()).toMatchSnapshot();
+   
 });
